46736768926036efa78c48e9dcc7a4a1eac7698d
avyudaya <avyudaya7@gmail.com>
2024-01-29 10:14:00 +0545

    added frontend

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect(config.MONGO_URI)
     .catch(err => logger.error(err.message));
 
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(requestLogger);
 
